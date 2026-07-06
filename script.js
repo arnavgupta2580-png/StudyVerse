6f614de107a53508193f28e4a32fe6a69e29e9e1
arnavgupta2580-png <arnavgupta2580@gmail.com>
2026-07-06 15:42:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Welcome to StudyVerse!");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    alert("Notes section will be available soon!");
+});
